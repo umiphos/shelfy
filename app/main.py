14354fa05de18b36a7e982be9fb05edc3afac1e7
umiphos <umiphos@gmail.com>
2026-09-03 11:58:38 -0600

    [ADD] add models and database creation(locally)

--- app/main.py
+++ app/main.py
@@ -2,14 +2,25 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+from .database import Base, engine
+
+from . import models
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="CATÁLOGO API")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 class RegisterRequest(BaseModel):
     email: str
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    password: Mapped[str] = mapped_column(String)
